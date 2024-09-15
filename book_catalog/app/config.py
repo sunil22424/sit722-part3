454d194937b99f0fe775982fccb723a0f6ec3720
sunil22424 <79262284+sunil22424@users.noreply.github.com>
2024-09-15 12:20:00 +1000

    Initial commit

---
+++ book_catalog/app/config.py
@@ -0,0 +1,3 @@
+import os
+
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://sit722task3_user:ePY0iq5NUHTb99dnHllVtQb5vCHdTr6l@dpg-crieusjv2p9s738h7nq0-a.oregon-postgres.render.com/sit722task3')
